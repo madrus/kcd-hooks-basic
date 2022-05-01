fed5ea085abcd71aae195614bffb1cb0c6bd4d48
Andre Roussakoff <madrus@gmail.com>
2022-05-01 19:54:41 +0200

    feat(Counter): add initialState and Step after lesson 3

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,8 +1,18 @@
 import { useState } from 'react'
 
-export const Counter = () => {
-  const [count, setCount] = useState<number>(0)
-  const increment = () => setCount(count + 1)
+export const useCounter = ({
+  initialState,
+  step
+}: {
+  initialState: number
+  step: number
+}) => {
+  const [count, setCount] = useState<number>(initialState)
+  const increment = () => setCount(count + step)
+  return { count, increment }
+}
 
+export const Counter = () => {
+  const { count, increment } = useCounter({ initialState: 2, step: 3 })
   return <button onClick={increment}>{count}</button>
 }
--- src/__tests__/Counter.spec.tsx
+++ src/__tests__/Counter.spec.tsx
@@ -7,10 +7,14 @@ import { Counter } from '../Counter'
 
 describe('<Counter />', () => {
   it('should increment the count', () => {
+    const initialState = 2
+    const step = 3
+
     const { container } = render(<Counter />)
     const button = container.firstChild as Element
-    expect(button?.textContent).toBe('0')
+
+    expect(button?.textContent).toBe('2')
     fireEvent.click(button)
-    expect(button?.textContent).toBe('1')
+    expect(button?.textContent).toBe('5')
   })
 })
