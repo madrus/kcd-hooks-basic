c435894ed4cdb44672c3af55d5060c07148262f8
Andre Roussakoff <madrus@gmail.com>
2022-05-01 22:30:31 +0200

    test: 💍 add test for saving to localStorage

--- src/__tests__/Counter.spec.tsx
+++ src/__tests__/Counter.spec.tsx
@@ -6,11 +6,25 @@ import { it, describe, expect, vi } from 'vitest'
 import { Counter } from '../Counter'
 
 describe('<Counter />', () => {
+  afterEach(() => {
+    window.localStorage.removeItem('count')
+  })
+
   it('should increment the count', () => {
     const { container } = render(<Counter />)
     const button = container.firstChild as Element
     expect(button?.textContent).toBe('0')
     fireEvent.click(button)
     expect(button?.textContent).toBe('1')
+  })
+
+  it('should read and write localStorage', () => {
+    window.localStorage.setItem('count', '3')
+    const { container } = render(<Counter />)
+    const button = container.firstChild as Element
+    expect(button?.textContent).toBe('3')
+    fireEvent.click(button)
+    expect(button?.textContent).toBe('4')
+    expect(window.localStorage.getItem('count')).toBe('4')
   })
 })
