c943a1496cd6323a002de9b7d775956bf333d261
Andre Roussakoff <madrus@gmail.com>
2022-05-01 22:13:02 +0200

    feat: 🎸 add useEffect to use localStorage

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,8 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const Counter = () => {
-  const [count, setCount] = useState<number>(0)
+  // making initialCount a function ensures that it runs only once the very first time!!!
+  const initialCount = () => Number(window.localStorage.getItem('count')) || 0
+  const [count, setCount] = useState<number>(initialCount)
   const increment = () => setCount(count + 1)
+  useEffect(() => {
+    window.localStorage.setItem('count', String(count))
+  }, [count])
 
   return <button onClick={increment}>{count}</button>
 }
