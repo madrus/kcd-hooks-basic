62a91e1cd4f9ed13dc3a03cbfb7a0d3f99ca120c
Andre Roussakoff <madrus@gmail.com>
2022-05-02 14:26:09 +0200

    refactor: 💡 rewrite the reducer w setState instead of dispatch

--- src/Stopwatch.tsx
+++ src/Stopwatch.tsx
@@ -1,41 +1,18 @@
 import { FC, useEffect, useReducer, useRef } from 'react'
 
 type StopwatchState = {
-  lapse: number
-  running: boolean
-}
-type StopwatchAction = {
-  type: 'LAPSE' | 'TOGGLE_RUNNING' | 'CLEAR'
-  now?: number
-  startTime?: number
+  lapse?: number
   running?: boolean
 }
 
-function reducer(state: StopwatchState, action: StopwatchAction) {
-  switch (action.type) {
-    case 'LAPSE':
-      return {
-        ...state,
-        lapse: action.now! - action.startTime!
-      }
-    case 'TOGGLE_RUNNING':
-      return {
-        ...state,
-        running: !state.running!
-      }
-    case 'CLEAR':
-      return {
-        ...state,
-        lapse: 0,
-        running: false
-      }
-    default:
-      return state
-  }
-}
+const reducer = (currentState: StopwatchState, newState: StopwatchState) => ({
+  ...currentState,
+  ...newState
+})
 
 const Stopwatch: FC = () => {
-  const [{ running, lapse }, dispatch] = useReducer(reducer, {
+  // we rename here dispatch to setState to make our intention clearer
+  const [{ running, lapse }, setState] = useReducer(reducer, {
     lapse: 0,
     running: false
   })
@@ -50,17 +27,17 @@ const Stopwatch: FC = () => {
     if (running) {
       clearInterval(intervalRef.current as NodeJS.Timeout)
     } else {
-      const startTime = Date.now() - lapse
+      const startTime = Date.now() - lapse!
       intervalRef.current = setInterval(() => {
-        dispatch({ type: 'LAPSE', now: Date.now(), startTime })
+        setState({ lapse: Date.now() - startTime })
       }, 0) // call the function a.s.a.p.
     }
-    dispatch({ type: 'TOGGLE_RUNNING' })
+    setState({ running: !running })
   }
 
   const handleClearClick = () => {
     clearInterval(intervalRef.current as NodeJS.Timeout)
-    dispatch({ type: 'CLEAR' })
+    setState({ lapse: 0, running: false })
   }
 
   return (
